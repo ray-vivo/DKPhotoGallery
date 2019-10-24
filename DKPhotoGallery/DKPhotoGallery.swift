ea0713cc83d953537254c8fed0165611c5a3111a
Ray Bryan Vivo <ray.vivo@pengower.co.uk>
2019-10-24 17:28:11 +0800

    added fix for ios 13 with target ios 11

--- DKPhotoGallery/DKPhotoGallery.swift
+++ DKPhotoGallery/DKPhotoGallery.swift
@@ -109,11 +109,14 @@ DKPhotoGalleryContentDataSource, DKPhotoGalleryContentDelegate {
         
         contentVC.footerView = self.footerView
         
-        let keyData = Data(bytes: [0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x42, 0x61, 0x72])
-        let key = String(data: keyData, encoding: String.Encoding.ascii)!
-        if let statusBar = UIApplication.shared.value(forKey: key) as? UIView {
-            self.statusBar = statusBar
+        if #available(iOS 13.0, *) {} else {
+            let keyData = Data([0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x42, 0x61, 0x72])
+            let key = String(data: keyData, encoding: String.Encoding.ascii)!
+            if let statusBar = UIApplication.shared.value(forKey: key) as? UIView {
+                self.statusBar = statusBar
+            }
         }
+
     }
     
     private lazy var doSetupOnce: () -> Void = {
